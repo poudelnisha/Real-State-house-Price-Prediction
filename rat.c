b8bc2d8b17acd0a4fbb3de2e57b987d6dfe9fa34
poudelnisha <52985004+poudelnisha@users.noreply.github.com>
2019-08-29 17:42:49 -0500

    Initial commit

---
+++ rat.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Nisha Poudel
+ * Date: 2019/08/27
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Nisha Nisha Nisha!\n");
+
+  return 0;
+}
